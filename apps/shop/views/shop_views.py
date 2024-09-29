5876453b32b6c58d74adc97c417048a33c1658d6
mangelryujin <mangelryujin@gmail.com>
2024-09-29 13:28:01 -0400

    create modified ui interface  in landing page

--- apps/shop/views/shop_views.py
+++ apps/shop/views/shop_views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from apps.general.models import *
 
 def shop_view(request):
-    return render(request,'shop_templates/shop.html')
+    response= render(request,'shop_templates/shop.html')
+    response['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response['Pragma'] = 'no-cache'
+    response['Expires'] = '0'
+    return response
