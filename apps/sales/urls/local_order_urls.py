582b6e66fbbe693902d930933286380e9474f357
mangelryujin <mangelryujin@gmail.com>
2024-10-01 11:23:07 -0400

    create search in local order

--- apps/sales/urls/local_order_urls.py
+++ apps/sales/urls/local_order_urls.py
@@ -4,6 +4,7 @@ from apps.sales.views.local_order_views import *
 urlpatterns = [
     path('',local_order_view,name='local_order_view'),
     path('local_order_results_view',local_order_results_view,name='local_order_results_view'),
+    path('local_order_create',local_order_create,name='local_order_create'),
     
     
 ]
