502d0a21028c3fa74ea58c71de931dbfa0c9b0ab
mangelryujin <mangelryujin@gmail.com>
2024-10-14 10:04:43 -0400

    create reviews form

--- apps/shop/urls/urls.py
+++ apps/shop/urls/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
-from apps.shop.views.product_detail import shop_product_detail_view
+from apps.shop.views.product_detail import create_product_review, shop_product_detail_view
 from apps.shop.views.products_store_views import *
 from apps.shop.views.shop_views import *
 
 urlpatterns = [
     path('',shop_view,name='shop_view'),
     path('products/',products_store_view,name='products_store_view'),
-    path('product-detail/',shop_product_detail_view,name='shop_product_detail_view'),
+    path('product-detail/<int:pk>/',shop_product_detail_view,name='shop_product_detail_view'),
     path('product_like/<int:pk>/',product_like,name='product_like'),
     path('product_cheap_like/<int:pk>/',product_cheap_like,name='product_cheap_like'),
     path('search_product/',search_product,name='search_product'),
     path('product_list_results/',product_list_results,name='product_list_results'),
+    path('create_product_review/<int:pk>/',create_product_review,name='create_product_review'),
     
     
 ]
